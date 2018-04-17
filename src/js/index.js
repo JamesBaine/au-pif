3d4a3ddb550aaa13ff45b1bb0a2ef91ab37272cf
JamesBaine <jamesmbaine@gmail.com>
2018-04-17 11:48:04 -0700

    initial api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+import axios from 'axios';
+
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '5440989e848a86a85c57e3fb09f6f22c';
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch(error) {
+        alert(error);
+    }
+    
+}
+getResults('pasta');
+
