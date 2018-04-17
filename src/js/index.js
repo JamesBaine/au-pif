5ad5fa26af4d9a2154086d99f396cfe8cf045104
JamesBaine <jamesmbaine@gmail.com>
2018-04-17 12:22:41 -0700

    search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,6 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '5440989e848a86a85c57e3fb09f6f22c';
-    try {
-        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch(error) {
-        alert(error);
-    }
-    
-}
-getResults('pasta');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
 
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '5440989e848a86a85c57e3fb09f6f22c';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch(error) {
+            alert(error);
+        }
+        
+    }
+}
+
+
